0d0dafe31dac42d8d0c4ab7a987f8855a9ae1004
Luis Colorado <luiscoloradourcola@gmail.com>
2019-11-08 08:28:43 +0200

    Add a default process function for each file
    Signed-off-by: Luis Colorado <luiscoloradourcola@gmail.com>

--- d2h.c
+++ d2h.c
@@ -15,7 +15,10 @@
 #include <string.h>
 #include <errno.h>
 
+#define F(_fmt) __FILE__":%d:%s: " _fmt, __LINE__, __func__
+
 #define EXIT_USAGE		(1)
+#define EXIT_FOPEN		(2)
 
 void do_usage(char *progname)
 {
@@ -23,11 +26,17 @@ void do_usage(char *progname)
 		"Usage: %1$s [ options ... ] [ file ... ]\n"
 		"\n",
 		progname);
-}
+} /* do usage */
+
+void process(char *fname, FILE *fdesc)
+{
+} /* process */
 
 int main(int argc, char **argv)
 {
 	int opt;
+	void (*prc)(char *fn, FILE *fd) = process;
+
 	while ((opt = getopt(argc, argv, "?")) != EOF) {
 		switch (opt) {
 		case '?':
@@ -36,6 +45,26 @@ int main(int argc, char **argv)
 			break;
 		} /* switch */
 	} /* while */
+
+	argc -= optind;
+	argv += optind;
+
+	if (argc > 0) {
+		int i;
+		for (i = 0; i < argc; i++) {
+			FILE *f = fopen(argv[i], "rt");
+			if (!f) {
+				fprintf(stderr,
+					F("fopen: %s: %s (errno = %d)\n"),
+					argv[i], strerror(errno), errno);
+				exit(EXIT_FOPEN);
+			}
+			prc(argv[i], f);
+			fclose(f);
+		}
+	} else {
+		prc("<<stdin>>", stdin);
+	}
 
 	exit(EXIT_SUCCESS);
 } /* main */
